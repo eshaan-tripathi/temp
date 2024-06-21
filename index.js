8cacb8df589aaa70f25f7f0c94289ace5d816ab3
Eshaan Tripathi <118004452+eshaan-tripathi@users.noreply.github.com>
2024-06-21 22:08:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send('Home');
-})
-app.get('/ok',(req,res)=>{
-    res.send('ok');
-})
-app.listen(3000,()=>{
-    console.log('Server Started');
-})
+app.get('/', (req, res) => {
+  res.send('Home');
+});
+
+app.get('/ok', (req, res) => {
+  res.send('ok');
+});
+
+module.exports = app;  // Export the app for Vercel serverless functions
+
+// No app.listen() is needed for Vercel deployment
